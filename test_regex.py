b3ca7aa33ad4e9e8c1e3515ed3a11e5b59e06602
PagesCoffy <isaac.p.coffie@gmail.com>
2023-06-05 14:39:45 +0000

    add regex

---
+++ test_regex.py
@@ -0,0 +1,34 @@
+"""
+Content of "log.txt":
+10.1.2.1 - car [01/Mar/2022:13:05:05 +0900] "GET /python HTTP/1.0" 200 2222
+10.1.1.9 - bike [01/Mar/2022:13:05:10 +0900] "GET /python HTTP/1.0" 200 2222
+
+Expected output:
+01/Mar/2022:13:05:05 +0900
+01/Mar/2022:13:05:10 +0900
+"""
+import re
+
+def parse1():
+    for line in open("log.txt"):
+        print(line.split("[")[1].split("]")[0])
+
+def parse2():
+    for line in open("log.txt", "r"):
+        print(line.split()[3].strip("[]"))
+
+def parse3():
+    for line in open("log.txt", "r"):
+        print(" ".join(line.split("[" or "]")[3:5]))
+
+def parse4():
+    for line in open("log.txt", "r"):
+        print(" ".join(line.split()[3:5]).strip("[]"))
+  
+def parse5():
+    for line in open("log.txt"):
+        print(re.split("\[|\]", line)[1])
+
+
+if __name__ == "__main__":
+    parse4()
